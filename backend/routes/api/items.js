86082fed037624bfa1ab91c7fea3c8300e163297
Itay Sidis <isidis@chegg.com>
2022-08-23 23:48:53 +0300

    adding title query filter to items api

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,7 +53,7 @@ router.get("/", auth.optional, function (req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
-  if (req.query.title) {
+  if (typeof req.query.title !== "undefined") {
     query.title = req.query.title;
   }
 

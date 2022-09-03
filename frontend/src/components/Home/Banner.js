99ab44776d71059dd41fd0ae28191beff713f560
Itay Sidis <isidis@chegg.com>
2022-09-03 13:44:51 +0300

    search-party. clicking on get reveals the input

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
+  const [showInput, setShowInput] = useState();
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            type="search"
-            id="search-box"
-            className="search-box"
-            placeholder="What is it that you truly desire?"
-            onChange={props.onSearchChange}
-          />
+          <span id="get-part">
+            A place to <span onClick={() => setShowInput(true)}>get</span>
+          </span>
+          {showInput && (
+            <input
+              type="search"
+              id="search-box"
+              className="search-box"
+              placeholder="What is it that you truly desire?"
+              onChange={props.onSearchChange}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>

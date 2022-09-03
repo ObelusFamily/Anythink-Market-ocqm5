e7be8fcd9c45ab608d202d342dd28c477959f20d
Itay Sidis <isidis@chegg.com>
2022-09-03 14:50:39 +0300

    search-party. clicking on get reveals the input

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -8,11 +8,9 @@ const Banner = (props) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">
-            A place to{" "}
-            <span id="get-part" onClick={() => setShowInput(true)}>
-              get
-            </span>
+          <span>A place to </span>
+          <span id="get-part" onClick={() => setShowInput(true)}>
+            get
           </span>
           {showInput && (
             <input

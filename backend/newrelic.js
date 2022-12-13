cd5cc0b005e523d01a258c45501d6faff8734afe
Itay Sidis <itaysidis@gmail.com>
2022-12-13 19:40:07 +0200

    observability - added new relic agent

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: "002c1552f2a93f74a1391a718db413da3715NRAL",
+  license_key: "",
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
